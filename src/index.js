d10a03b674b8d4d7064659ebb4238e59ac0d0aa9
alygeha <geha.ali@gmail.com>
2017-08-30 13:55:51 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-/* eslint-disable no-console */
-console.log('hi');
+import 'babel-polyfill'; //in real app case , use only the polyfill that you need, babel-polyfill transpile some special syntax such as routes
+import React from 'react';
+import { render } from 'react-dom'; //in react.14 react-DOM was split off from React, we need here to use render function
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const store = configureStore();
+
+//NB : render as a function here with 2 params
+render (
+      <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+      </Provider>,
+      document.getElementById('app')
+);
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,19 +5,19 @@ export default {
   debug: true,
   devtool: 'inline-source-map',
   noInfo: false,
-  entry: [
+  entry: [ //defining the entry points (bootstrap)
     'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
     path.resolve(__dirname, 'src/index')
   ],
-  target: 'web',
+  target: 'web', //it could be 'node'
   output: {
     path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
     publicPath: '/',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'src')
+    contentBase: path.resolve(__dirname, 'src') //the source code for webpack's dev server
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -27,7 +27,7 @@ export default {
     loaders: [
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
       {test: /(\.css)$/, loaders: ['style', 'css']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'}, //these files are used by Bootstrap for fonts
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
       {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
